54955e9ab218759944695b44bbb04c838ae1fd15
codecorner1983 <pallavikarnati@itformula1.com>
2025-06-05 13:17:07 +0530

    index.html

--- application/language/français/pagination_lang.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$lang['first'] = 'Premier';
-$lang['last'] = 'Dernier';
-$lang['next'] = 'Suivant';
-$lang['previous'] = 'Précédent';
--- application/language/indonesian/pagination_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$lang['first'] = 'Pertama';
-$lang['last'] = 'Terakhir';
-$lang['next'] = 'Selanjutnya';
-$lang['previous'] = 'Sebelumnya';
